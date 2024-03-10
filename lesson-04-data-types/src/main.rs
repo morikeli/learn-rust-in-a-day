d5411b5885261fb817c8a80a34006f5aa153bc0d
brocode <kelimbth@gmail.com>
2024-03-10 12:06:52 +0300

    completed lesson 4 ✅

---
+++ lesson-04-data-types/src/main.rs
@@ -0,0 +1,17 @@
+// Lesson 4 - variable data types
+fn main() {
+    // i32 - makes a variable a 32-bit signed integer
+    // u32 - makes a variable a 32-bit unsigned integer
+    // i64 - makes a variable a 64-bit signed integer
+    // u64 - makes a variable a 64-bit unsigned integer
+    // f32 - used in floating numbers in this case a 32-bit
+
+    // NOTE: unsigned integers don't support negative numbers.
+
+    let x: i64 = 45;    // i64
+    let f: f32 = 6.7;    // f32
+    let b: bool = false;
+
+    print!("Value of x: {} | Value of f: {} | Value of b: {}", x, f, b)
+    
+}
